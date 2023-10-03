fb1fc44546b13d0f6c300f2ec7760752190f2142
yuvalsol <yuvalsol@gmail.com>
2023-10-03 21:23:56 +0300

    Disclaimer better wording

--- POCOGenerator/Disclaimer.cs
+++ POCOGenerator/Disclaimer.cs
@@ -4,11 +4,10 @@ namespace POCOGenerator
 {
     public static class Disclaimer
     {
-        public static readonly string Message = string.Join(Environment.NewLine,
-            "Several people reported data loss after using this utility.",
-            "Some tables were cleared of all their records but they were able to restore them from backup.",
-            "This bug is NOT resolved despite my efforts to replicate and solve it.",
-            "Backup your databases before using this utility or use it at your own peril."
-        );
+        public static readonly string Message =
+            @"One person reported data loss after using this utility (Comments section in the original article on CodeProject).
+Some tables were cleared of all their records but they were able to restore them from backup.
+This bug is NOT resolved despite my efforts to replicate and solve it.
+Backup your database before using this utility or use it at your own risk.";
     }
 }
